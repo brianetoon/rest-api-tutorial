8bb75149af3bfb15600da9d67f51ae5c397934bc
BrianToon <brian.elliot.toon@gmail.com>
2017-11-06 18:11:34 -0500

    updated post in api.js updated index.js to use body-parser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const routes = require('./routes/api');
+const bodyParser = require('body-parser');
 
 // set up express app
 const app = express();
 
+app.use(bodyParser.json());
+
+// initialize routes
 app.use('/api', routes);
 
 // listen for requests
